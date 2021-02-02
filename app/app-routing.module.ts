9cfeae15ffc6a79755c342ec52e9afa401288e83
pri12368 <74955915+pri12368@users.noreply.github.com>
2021-02-02 16:53:10 +0530

    Add files via upload

---
+++ app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { LoginsuccessComponent } from './loginsuccess/loginsuccess.component';
+
+const routes: Routes = [
+  {path:'',component:LoginComponent},
+  {path:'loginsuccess',component:LoginsuccessComponent},
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
